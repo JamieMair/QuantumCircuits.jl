d3cbc44648bb496faa491c740f45bb27eb352b57
Jamie Mair <JamieMair@users.noreply.github.com>
2023-10-16 21:23:05 +0100

    Simplified the examples

--- examples/test_contractions.jl
+++ examples/test_contractions.jl
@@ -2,40 +2,16 @@ using Revise
 using QuantumCircuits
 using Test
 using Random
-includet("functions.jl")
-
-gatebits(gate::QuantumCircuits.Abstract1SpinGate) = 1
-gatebits(gate::QuantumCircuits.Abstract2SpinGate) = 2
-mat(gate) = QuantumCircuits.mat(gate)
-mat(gate::QuantumCircuits.Abstract2SpinGate) = reshape(QuantumCircuits.mat(gate), 4, 4)
-getval(::Val{X}) where {X} = X
-function convert_gates_to_matrix(nbits, gates)
-    gate_dict = Dict{Int, QuantumCircuits.AbstractGate}(
-        (getval(g.gate_dim_val)=>g for g in gates)...
-    )
-    out_mats = []
-    i = 1
-    while i <= nbits
-        if haskey(gate_dict, i)
-            push!(out_mats, mat(gate_dict[i]))
-            i += gatebits(gate_dict[i])
-        else
-            push!(out_mats, mat(QuantumCircuits.IdentityGate()))
-            i += 1
-        end
-    end
-    return operation_tensor(out_mats...)
-end
-
+using LinearAlgebra
 
 nbits = 4
 random_unitary = randn(2,2,2,2)
 random_unitary ./= norm(random_unitary)
 
 gates = [
-    Localised1SpinGate(QuantumCircuits.HadamardGate(), Val(1)),
-    Localised2SpinAdjGate(QuantumCircuits.Generic2SpinGate(random_unitary), Val(2)),
-    Localised1SpinGate(QuantumCircuits.HadamardGate(), Val(4))
+    Localised1SpinGate(HadamardGate(), Val(1)),
+    Localised2SpinAdjGate(Generic2SpinGate(random_unitary), Val(2)),
+    Localised1SpinGate(HadamardGate(), Val(4))
 ];
 
 equiv_operator = convert_gates_to_matrix(nbits, gates);
@@ -50,3 +26,4 @@ out_tensor_vec = reshape(out_tensor, 2^nbits);
 out_vec = equiv_operator * psi_vec;
 
 @test real.(out_tensor_vec.*adjoint.(out_tensor_vec)) ≈ real.(out_vec.*adjoint.(out_vec))
+@test out_tensor_vec ≈ out_vec
