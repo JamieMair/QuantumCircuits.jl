4a617cfb3015d5f9eb4a116b4c543829fa59c1ea
Jamie Mair <JamieMair@users.noreply.github.com>
2023-10-24 17:27:33 +0100

    Refactored gates to a separate file and added rotation gates

--- src/QuantumCircuits.jl
+++ src/QuantumCircuits.jl
@@ -11,56 +11,10 @@ export Localised1SpinGate, Localised2SpinAdjGate
 export CNOTGate, XGate, ZGate, IdentityGate, HadamardGate, Generic1SpinGate, Generic2SpinGate
 export convert_gates_to_matrix
 export zero_state_vec, zero_state_tensor
-
+export build_general_unitary_gate
 
 ## GATES
-abstract type AbstractGate end
-
-abstract type Abstract1SpinGate <: AbstractGate end
-abstract type Abstract2SpinGate <: AbstractGate  end
-
-function mat(x::AbstractGate) 
-    error("Unimplemented matrix representation for $(typeof(x))")
-end
-
-const _HadamardGateMat = SMatrix{2, 2, Float64, 4}([1; 1;; 1; -1]' ./ sqrt(2));
-const _IdentityGateMat = SMatrix{2, 2, Float64, 4}([1; 0;;0;1]');
-const _XGateMat = SMatrix{2, 2, Float64, 4}([0; 1;;1; 0]');
-const _ZGateMat = SMatrix{2, 2, Float64, 4}([1; 0;;0; -1]');
-const _IGateMat = SMatrix{2, 2, Float64, 4}([1; 0;;0; 1]');
-const _CNOTMat = reshape(SMatrix{4,4, Float64, 16}([1;0;0;0;;0;0;0;1;;0;0;1;0;;0;1;0;0]), 2,2,2,2);
-
-struct XGate <: Abstract1SpinGate end
-mat(::XGate) = _XGateMat
-struct ZGate <: Abstract1SpinGate end
-mat(::ZGate) = _ZGateMat
-struct HadamardGate <: Abstract1SpinGate end
-mat(::HadamardGate) = _HadamardGateMat
-struct IdentityGate <: Abstract1SpinGate end
-mat(::IdentityGate) = _IGateMat
-struct CNOTGate <: Abstract2SpinGate end
-mat(::CNOTGate) = _CNOTMat
-
-struct Generic1SpinGate{T, AT<:AbstractArray{T,2}} <: Abstract1SpinGate
-    array::AT
-end
-mat(g::Generic1SpinGate) = g.array
-struct Generic2SpinGate{T, AT<:AbstractArray{T,4}} <: Abstract2SpinGate
-    array::AT
-end
-mat(g::Generic2SpinGate) = g.array
-
-struct Localised1SpinGate{G<:Abstract1SpinGate, K} <: Abstract1SpinGate
-    gate::G
-    gate_dim_val::Val{K}
-end
-mat(l::Localised1SpinGate) = mat(l.gate)
-# NOTE: Acts on spin K and K+1
-struct Localised2SpinAdjGate{G<:Abstract2SpinGate, K} <: Abstract2SpinGate
-    gate::G
-    gate_dim_val::Val{K}
-end
-mat(l::Localised2SpinAdjGate) = mat(l.gate)
+include("gates.jl")
 
 ## FUNCTIONS
 gatebits(gate::QuantumCircuits.Abstract1SpinGate) = 1
