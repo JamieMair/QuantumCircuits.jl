aaa49cf38fe0682b22b4b8ff79653bdbdc12ebea
Jamie Mair <JamieMair@users.noreply.github.com>
2023-11-20 16:29:39 +0000

    Added some code to plot to fix and plot the HPC results

--- examples/barren_plateau.jl
+++ examples/barren_plateau.jl
@@ -54,7 +54,7 @@ function plot_var_grads_vs_layers(nbits, nlayers, hamiltonian_params, results)
         title=layer_plot_title(hamiltonian_params),
         xlabel="# layers",
         ylabel=L"\text{var}\left ([\nabla E]_1 \right )",
-        yscale=Makie.pseudolog10)
+        yscale=log10)
 
     for (i, nb) in enumerate(nbits)
         layer_grads = view(results, :, i, :)
@@ -80,4 +80,78 @@ results = experiment(nbits, nlayers, nrepeats, hamiltonian_params)
 plot_var_grads_vs_layers(nbits, nlayers, hamiltonian_params, results)
 
 
-# TODO: Write a script to run this on orpheus
+# Get results from HPC
+using Experimenter
+import Serialization
+using DataFrames
+using SQLite
+db_path = joinpath(@__DIR__, "hpc/results/barren_plateau_experiments.db");
+file_name = splitpath(db_path)[end];
+db = open_db(file_name, dirname(db_path));
+function _deserialize_columns(df::DataFrame)
+    for colname in names(df)
+        col = df[!, colname]
+        if eltype(col) <: Vector{UInt8} # raw binary
+            df[!, colname] = map(col) do c
+                io = IOBuffer(c)
+                return (Serialization.deserialize(io)).object
+            end
+        elseif eltype(col) <: SQLite.Serialized
+            df[!, colname] = map(x->x.object, col)
+        end
+    end
+    return df
+end
+function custom_get_trials(db::ExperimentDatabase, name)
+    sql = raw"""
+    SELECT name, Trials.id as id, experiment_id, Trials.configuration as configuration, results, trial_index, has_finished 
+    FROM Trials 
+    INNER JOIN Experiments ON Experiments.id == Trials.experiment_id 
+    WHERE name = ? 
+    ORDER BY trial_index
+    """
+    df = _deserialize_columns(SQLite.DBInterface.execute(db._db, sql, (name,)) |> DataFrame)
+    return [Trial(row) for row in eachrow(df)]
+end
+trials = custom_get_trials(db, "Barren Plateau Vanilla");
+
+nbits = sort([Set([t.configuration[:nbits] for t in trials])...])
+nlayers = sort([Set([t.configuration[:nlayers] for t in trials])...])
+
+hamiltonian_params = begin
+    J = sort([Set([t.configuration[:J] for t in trials])...])
+    h = sort([Set([t.configuration[:h] for t in trials])...])
+    g = sort([Set([t.configuration[:g] for t in trials])...])
+    @assert length(J) == 1
+    @assert length(h) == 1
+    @assert length(g) == 1
+    hamiltonian_params = (;
+    J=J[begin],
+    h=h[begin],
+    g=g[begin])
+    return hamiltonian_params
+end
+
+begin
+    gate_index = sort([Set([t.configuration[:gate_index] for t in trials])...])
+    @assert length(gate_index) == 1
+    gate_index = gate_index[begin]
+end
+
+begin
+    nrepeats = sort([Set([t.configuration[:nrepeats] for t in trials])...])
+    @assert length(nrepeats) == 1
+    nrepeats = nrepeats[begin]
+end
+
+results = begin
+    results = zeros(Float64, (nrepeats, length(nbits), length(nlayers)))
+
+    for ((i, nb), (j, nl)) in Iterators.product(enumerate(nbits), enumerate(nlayers))
+        t = first([t for t in trials if t.configuration[:nlayers] == nl && t.configuration[:nbits] == nb])
+        results[:, i, j] .= t.results[:gradients]
+    end
+    return results
+end
+
+plot_var_grads_vs_layers(nbits, nlayers, hamiltonian_params, results)
