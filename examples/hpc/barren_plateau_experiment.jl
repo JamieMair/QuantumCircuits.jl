de5624f9dddfa5798b2de3f45f84f192a9a7bd5c
Jamie Mair <JamieMair@users.noreply.github.com>
2023-11-20 12:58:32 +0000

    Updated configuration

--- examples/hpc/barren_plateau_experiment.jl
+++ examples/hpc/barren_plateau_experiment.jl
@@ -4,9 +4,9 @@ cd(joinpath(@__DIR__, ".."))
 db = open_db("experiments.db", "hpc/results", true)
 
 config = Dict{Symbol, Any}(
-    :nbits => IterableVariable(collect(4:2:6)),
-    :nlayers => IterableVariable(collect(2:2:10)),
-    :nrepeats => 100,
+    :nbits => IterableVariable(collect(4:2:12)),
+    :nlayers => IterableVariable(collect(2:2:80)),
+    :nrepeats => 10000,
     :gate_index => 1,
     :J => 1.0,
     :h => 0.5,
@@ -14,7 +14,7 @@ config = Dict{Symbol, Any}(
 )
 
 experiment = Experiment(
-    name="Barren Plateau Vanilla (Test)",
+    name="Barren Plateau Vanilla",
     include_file="hpc/barren_plateau_trial.jl",
     function_name="run_trial",
     configuration = deepcopy(config)
