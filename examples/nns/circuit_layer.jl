b3923f41c9c8d752e442978edf8985a0dec5957b
Jamie Mair <JamieMair@users.noreply.github.com>
2024-01-09 13:09:24 +0000

    Added cuda to test circuit

--- examples/nns/circuit_layer.jl
+++ examples/nns/circuit_layer.jl
@@ -2,6 +2,8 @@ using QuantumCircuits
 using ChainRulesCore
 using Flux
 using ProgressBars
+using CUDA
+
 include("../test_brickwork_problem.jl")
 
 struct HamiltonianLayer{T,A<:AbstractArray}
@@ -23,7 +25,24 @@ function apply_hamiltonian(hl::HamiltonianLayer, gate_angles::AbstractArray)
     
     E = QuantumCircuits.measure(hl.H, hl.ψ₀, circuit);
     return E
+end
+function apply_hamiltonian(hl::HamiltonianLayer, gate_angles::CuArray)
+    return apply_hamiltonian(hl, Array(gate_angles))
+end
 
+function ChainRulesCore.rrule(::typeof(apply_hamiltonian), hl::HamiltonianLayer, gate_angles::CuArray)
+    angles_cpu = Array(gate_angles)
+    E = apply_hamiltonian(hl, angles_cpu)
+    function pb(dE)
+        gs = if dE == 1
+            ChainRulesCore.@thunk(Flux.gpu(QuantumCircuits.gradients(hl.H, hl.ψ₀, GenericBrickworkCircuit(hl.nbits, hl.nlayers, hl.ngates, angles_cpu))))
+        else
+            ChainRulesCore.@thunk(Flux.gpu(Array(dE) .* QuantumCircuits.gradients(hl.H, hl.ψ₀, GenericBrickworkCircuit(hl.nbits, hl.nlayers, hl.ngates, angles_cpu))))
+        end
+
+        return NoTangent(), NoTangent(), gs
+    end
+    return E, pb
 end
 
 function ChainRulesCore.rrule(::typeof(apply_hamiltonian), hl::HamiltonianLayer, gate_angles::AbstractArray)
@@ -42,9 +61,19 @@ end
 
 Flux.@functor HamiltonianLayer
 
-function test_network()
-    nbits = 6
-    nlayers = 4
+function HamiltonianLayer(nbits::Int, nlayers::Int, ngates::Int, ψ₀::CuArray, H::CuArray)
+    return HamiltonianLayer(nbits, nlayers, ngates, Array(ψ₀), Array(H))
+end
+function HamiltonianLayer(nbits::Int, nlayers::Int, ngates::Int, ψ₀::Array, H::CuArray)
+    return HamiltonianLayer(nbits, nlayers, ngates, ψ₀, Array(H))
+end
+function HamiltonianLayer(nbits::Int, nlayers::Int, ngates::Int, ψ₀::CuArray, H::Array)
+    return HamiltonianLayer(nbits, nlayers, ngates, Array(ψ₀), H)
+end
+
+function test_network(;use_gpu = true)
+    nbits = 10
+    nlayers = 10
     ngates = QuantumCircuits.brickwork_num_gates(nbits, nlayers)
     nangles = ngates * 15
     ψ₀ = QuantumCircuits.zero_state_tensor(nbits)
@@ -53,21 +82,23 @@ function test_network()
     g = 0;
     H = build_hamiltonian(nbits, J, h, g);
     model = Chain(
-        Dense(1=>40, tanh),
-        Dense(40=>40, tanh),
-        Dense(40=>nangles, Flux.σ),
+        Dense(1=>50, tanh),
+        Dense(50=>50, tanh),
+        Dense(50=>nangles, Flux.σ),
         x -> x .* (2π),
         x -> reshape(x, 15, ngates),
         HamiltonianLayer(nbits, nlayers, ngates, ψ₀, H),
         E -> sum(E)
     )
+    use_gpu && return model |> Flux.gpu
     return model
 end
 
-function train!(network, epochs)
-    input = [1.0f0];
+function train!(network, epochs; lr=0.005, use_gpu = true)
+    input = use_gpu ? [1.0f0] |> Flux.gpu : [1.0f0];
+    
     losses = Float32[];
-    optim = Flux.setup(Flux.Adam(0.01), network);
+    optim = Flux.setup(Flux.Adam(lr), network);
     for e in ProgressBar(1:epochs)
         energy, grads = Flux.withgradient(network) do m
             m(input)
