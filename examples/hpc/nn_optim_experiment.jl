e375cc7c8ee35b78a085b395b238f30521434686
Jamie Mair <JamieMair@users.noreply.github.com>
2024-01-17 14:31:38 +0000

    Change to larger neural network

--- examples/hpc/nn_optim_experiment.jl
+++ examples/hpc/nn_optim_experiment.jl
@@ -5,23 +5,23 @@ db = open_db("experiments.db", "hpc/results", true)
 
 nrepeats = 10
 config = Dict{Symbol,Any}(
-    :nbits => IterableVariable(collect(4:2:12)),
-    :nlayers => IterableVariable([4, 8, 12, 16, 20, 24, 28, 32, 36, 40]),
+    :nbits => IterableVariable(collect(4:2:8)),
+    :nlayers => IterableVariable([4, 8, 12, 16]),
     :repeat_id => IterableVariable(collect(1:nrepeats)),
     :J => 1.0,
     :h => 0.5,
     :g => 0.0,
     :use_gpu => true,
     :architecture => [
-        (; neurons=50, activation=:tanh),
-        (; neurons = 50, activation = :tanh),
-        (; neurons=50, activation=:tanh)
+        (; neurons=250, activation=:tanh),
+        (; neurons = 250, activation = :tanh),
+        (; neurons=250, activation=:tanh)
     ],
     :epochs => 100,
 )
 
 experiment = Experiment(
-    name="NN Optimisation 1",
+    name="NN Optimisation 2",
     include_file="hpc/nn_optim_trial.jl",
     function_name="run_trial",
     configuration=deepcopy(config)
