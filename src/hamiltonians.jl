5776325c3ff3eed796898400e23abbdf555b98b7
Jamie Mair <JamieMair@users.noreply.github.com>
2024-03-14 10:51:04 +0000

    Allowed the TFIM to use more parameters

--- src/hamiltonians.jl
+++ src/hamiltonians.jl
@@ -1,6 +1,15 @@
+"""
+A struct containing the parameters of the Transverse Field Ising Model.
+
+The form of H represented by this struct is
+```math
+\\mathcal{H} = -J \\left ( \\sum_{\\langle i,j\\rangle} \\sigma_z^{(i)}\\sigma_z^{(j)} + g \\sum_i \\sigma_z^{(i)} + h \\sum_i \\sigma_x^{(i)} \\right ).
+```
+"""
 struct TFIMHamiltonian{T}
-    J::T
-    g::T
+    J::T # Coupling (not factored out)
+    h::T # Transverse field
+    g::T # Longitudinal field
 end
 
 @kernel function _tfim_measure!(ψ′, @Const(H), @Const(ψ), ::Val{N}) where {N}
@@ -10,21 +19,20 @@ end
 
     @inbounds psi = ψ[idx]
 
-    # Loop over adjacent pairs
+    # Loop over adjacent pairs to calculate zz
     zz = zero(Int8) # Allows up to -127 bits -> will be more than enough
     for i in T(0):T(N-2)
         r = unsafe_trunc(UInt8, C >> i) % 0b0100
         # Matching spins
-        zz += r == 0b11
-        zz += r == 0b00
+        zz += (r == 0b11) || (r == 0b00)
         # Different spins
-        zz -= r == 0b01
-        zz -= r == 0b10
+        zz -= (r == 0b01) || (r == 0b10)
     end
 
     # Nearest neighbour contribution
     nn_contribution = zz * abs2(psi)
 
+    # Loop over bits to calculate x term
     x = zero(eltype(ψ))
     @inbounds for i in T(0):T(N-1)
         C_flip = xor(C, one(T) << i)
@@ -33,8 +41,12 @@ end
     end
     tf_contribution = x * psi
 
+    # Find the number of spin up and spin down
+    n_ones = count_ones(C)
+    z_contribution = (2n_ones - N)
 
-    @inbounds ψ′[idx] = (H.g * tf_contribution - H.J * nn_contribution)
+
+    @inbounds ψ′[idx] = -H.J * (H.h * tf_contribution + H.g * z_contribution + nn_contribution)
     nothing
 end
 
