b290e210cacf5ae2aff8c292f7b05d9f0cb83161
Jamie Mair <JamieMair@users.noreply.github.com>
2024-04-04 13:57:59 +0000

    Added a plot for comparing gradient during training

--- examples/nns/utils.jl
+++ examples/nns/utils.jl
@@ -177,6 +177,125 @@ function plot_all_energy_trajectories(dfs...; plot_log=false)
     return f
 end
 
+
+function plot_gradient_norms(dfs...; plot_log=false)
+    nbits_set = sort(unique(vcat((unique(df[!, :c_nbits]) for df in dfs)...)))
+
+    f = Figure(size=(300 * length(dfs), 300 * length(nbits_set)))
+
+    nlayers_set = sort(unique(vcat((unique(df[!, :c_nlayers]) for df in dfs)...)))
+    min_layers, max_layers = extrema(vcat(([extrema(df[!, :c_nlayers])...] for df in dfs)...))
+    m = log(256 / 1) / log(max_layers / min_layers)
+    c = log(1) - m * log(min_layers)
+    convert_col_to_idx(nl) = Int(257 - round((1 + sqrt((nl - min_layers) / (max_layers - min_layers)) * 255)))
+    color_scheme = ColorSchemes.matter
+
+    color_map = Dict((
+        map(nlayers_set) do nl
+            index = convert_col_to_idx(nl)
+            # i = max(1, min(256, Int(round(exp(m*log(nl)+c)))))
+            return nl => color_scheme[index]
+        end
+    )...)
+
+    ground_energy_fn = MemoisedGroundStateFn(Float64)
+    
+
+    # Map each row to a list of axes to align them later
+    rows_axes = Dict{Int,Any}()
+
+    for (j, df) in enumerate(dfs)
+
+        nbits_local_set = Set(sort(unique(df[!, :c_nbits])))
+        nlayers_local_set = reverse(sort(unique(df[!, :c_nlayers])))
+
+
+
+        for (i, nbits) in enumerate(nbits_set)
+
+            max_energy = -Inf
+
+            if !(nbits in nbits_local_set)
+                continue # Skip over graphs that don't exist
+            end
+
+            current_entries = df[!, :c_nbits] .== nbits
+            archs = unique(df[current_entries, :c_architecture])
+            @assert length(archs) == 1 "There should be only be a single architecture."
+
+            nparams = length(destructure(df[current_entries, :r_model_state][1])[1])
+            nparams_log10 = Int(ceil(log10(nparams)))
+            additional_args = plot_log ? Dict{Symbol,Any}(
+                # :xscale => log10,
+                :yscale => log10,
+            ) : Dict{Symbol,Any}()
+            xlabel = i < length(nbits_set) ? "" : L"t"
+            ylabel = j > 1 ? "" : L"\mathbb{E} \left [ \left {||} \overline{\nabla_\theta H} \right {||} \right ]"
+            ax = Axis(f[i, j]; xlabel, ylabel, title=LaTeXString("\$n=$nbits, |\\theta|\\approx 10^{$nparams_log10}\$"), additional_args...)
+
+            if haskey(rows_axes, i)
+                push!(rows_axes[i], ax)
+            else
+                rows_axes[i] = [ax]
+            end
+
+            J = first(unique(df[current_entries, :c_J]))
+            h = first(unique(df[current_entries, :c_h]))
+            g = first(unique(df[current_entries, :c_g]))
+
+            ground_energy, _ = ground_energy_fn(nbits, J, h, g)
+            # hlines!(ax, ground_energy, label=L"E_0", linestyle=:dash, alpha=0.5, color=:black)
+
+            for nlayers in nlayers_local_set
+                trajectories = map(subset(df,
+                    :c_nbits => nb -> nb .== nbits,
+                    :c_nlayers => nl -> nl .== nlayers)[!, :r_training_info]) do info
+                        epochs = sort([k for k in keys(info[:gradient_info])])
+                        return map(epochs) do i
+                            info[:gradient_info][i].norm_grads # assume same epochs
+                        end
+                end
+
+                c = color_map[nlayers]
+
+
+                combined_trajs = hcat(trajectories...)
+                if length(combined_trajs) == 0
+                    continue
+                end
+                mean_trajectory = reshape(mean(combined_trajs, dims=2), :)
+                err_trajectory = reshape(std(combined_trajs, dims=2), :) / sqrt(length(trajectories))
+                epochs = 0:(length(mean_trajectory)-1)
+
+                max_epoch = argmax(mean_trajectory)
+
+
+                band_c = RGBAf(c.r, c.g, c.b, 0.2)
+                band!(ax, epochs, mean_trajectory .- err_trajectory, mean_trajectory .+ err_trajectory; alpha=0.2, label=nothing, color=band_c)
+
+                lines!(ax, epochs, mean_trajectory; label="$nlayers", color=c, lw=3)
+                
+                xlims!(ax, 0, maximum(epochs))
+            end
+
+            # ylims!(ax, 0.0000000001, max_energy)
+        end
+
+    end
+    new_colour_scheme = ColorScheme(map(LinRange(min_layers, max_layers, 256)) do nl
+        color_scheme[convert_col_to_idx(nl)]
+    end)
+
+    for row in keys(rows_axes)
+        linkyaxes!(rows_axes[row]...)
+    end
+
+
+    cbar = Colorbar(f[length(nbits_set)+1, 1:length(dfs)], limits=(min_layers, max_layers), ticks=nlayers_set, colormap=new_colour_scheme, vertical=false, label="Layers")
+
+    return f
+end
+
 function plot_barren_plateaux(dfs...; plot_log=true)
 
     nbits_set = sort(unique(vcat((unique(df[!, :c_nbits]) for df in dfs)...)))
@@ -238,7 +357,7 @@ function plot_barren_plateaux(dfs...; plot_log=true)
         ) : Dict{Symbol,Any}()
 
 
-        ylabel = j > 1 ? "" : L"\mathbb{E} \left [ \frac{ \left {||} \overline{\nabla_\theta} \right {||} }{N_\text{params}} \right ]"
+        ylabel = j > 1 ? "" : L"\mathbb{E} \left [ \left {||} \overline{\nabla_\theta} \right {||} \right ]"
         ax = Axis(f[1, j]; xlabel=L"l", ylabel, title=labels[j], yscale=log10)
         push!(row_axs, ax)
 
@@ -258,11 +377,13 @@ function plot_barren_plateaux(dfs...; plot_log=true)
                 tmp_df = subset(df,
                     :c_nbits => nb -> nb .== nbits,
                     :c_nlayers => nl -> nl .== nlayers)
-
+                
+                nparams = QuantumCircuits.brickwork_num_gates(nbits, nlayers) * 15
+                
                 grads = map(tmp_df[!, :r_training_info]) do info
                     k = info[:gradient_info][1]
-                    nparams = QuantumCircuits.brickwork_num_gates(nbits, nlayers) * 15
-                    return k.norm_grads / nparams
+                    
+                    return k.norm_grads
                 end
 
                 if length(grads) == 0
@@ -296,7 +417,6 @@ function plot_barren_plateaux(dfs...; plot_log=true)
 
     return f
 end
-
 
 function count_neurons(df)
     return length(df.c_architecture[1]) == 0 ? 0 : sum(y -> y.neurons, df.c_architecture[1])
