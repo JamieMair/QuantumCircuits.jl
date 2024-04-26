26adc732eda967133ceec47836ba9ec4eda069df
Adam Smith <adam.smith.home@ntlworld.com>
2024-04-26 11:02:39 +0100

    Add MPS version of circuit optimisation

--- examples/test_optimise_mps.jl
+++ examples/test_optimise_mps.jl
@@ -1,6 +1,6 @@
 using Revise
 using QuantumCircuits
-
+using MatrixProductStates
 
 import KrylovKit: eigsolve
 
@@ -17,15 +17,22 @@ N = 14  # even!
 M = 4  # even! I will be counting layers as two of Jamie's layers so that there are N-1 gates in a layer.
 n_layers = M ÷ 2
 
+
+H_mpo = TFIM(N, -J, -g, -h);
+
 H_sparse = build_sparse_tfim_hamiltonian(N, J, h, g);
 eigen_vals, eigen_vecs, _ = eigsolve(H_sparse, 2^N, 1, :SR);
 energy_GS = first(eigen_vals)
 psi_GS = first(eigen_vecs);
 
-U_id = reshape(Matrix{ComplexF64}(I, 4, 4), (2,2,2,2));
+psi_GS_mps = MatrixProductStates.vector2MPS((1.0+0.0im)*psi_GS, 2, 0, 0.0);  # exact MPS
+
+expectation(psi_GS_mps, H_mpo)
+
+U_id = Matrix{ComplexF64}(I, 4, 4);
 circuit = [[copy(U_id) for i in 1:N-1] for j in 1:n_layers];
 
-@time circuit, overlaps, energies = polar_optimise(circuit, psi_GS, H_sparse, N, iterations=500);
+@time circuit, overlaps, energies = polar_optimise_mps(circuit, psi_GS_mps, H_mpo, N, iterations=500);
 
 
 using Plots
@@ -35,21 +42,4 @@ plot((real(energies) .- energy_GS) ./ abs(energy_GS), yaxis=:log, label="N=$N, M
 xlabel!("iteration")
 ylabel!(L"$\frac{\langle H \rangle - E}{E}$")
 
-#savefig("overlap_optimisation.pdf") 
-
-
-
-using MatrixProductStates
-
-circuit_matrices = [[reshape(permutedims(gate,(2,1,4,3)),(4,4)) for gate in layer] for layer in circuit];
-
-psi_mps = QuantumCircuits.circuit_to_mps(circuit_matrices, N)
-psi_mps_flat = flatten(psi_mps)
-
-psi_GS[:]' * psi_mps_flat
-psi_mps_flat[:]' * H_sparse * psi_mps_flat
-psi_mps_flat[:]' * psi_mps_flat
-
-H_mpo = TFIM(N, -J, -g, -h);
-
-expectation(psi_mps, H_mpo)
+savefig("overlap_optimisation.pdf") 
--- src/polar_optimise.jl
+++ src/polar_optimise.jl
@@ -3,6 +3,7 @@ using MatrixProductStates  # should be moved to QuantumCircuits.jl when happy
 using LinearAlgebra
 using SparseArrays
 using ProgressBars
+using TensorOperations
 
 import KrylovKit: eigsolve
 
@@ -198,6 +199,144 @@ function polar_optimise(circuit, psi_GS, H_sparse, N; iterations=100)
 
     end
 
-    return overlaps, energies
+    return circuit, overlaps, energies
+
+end
+
+
+"""
+Move to MatrixProductStates.jl when happy
+"""
+function conj_mps(mps::MPS)
+    N = mps.N
+    mps_conj = MPS(mps.d, mps.N, copy(mps.tensors), mps.centre, mps.chiMax, mps.threshold)
+
+    for site in 1:N
+        mps_conj[site] = conj(mps[site])
+    end
+
+    return mps_conj
+end
+
+function circuit_to_mps(circuit, N)
+    """
+    NOTE: gate is a 4x4 matrix here! Different to other state vector version
+    Should fix one convention when tested.
+    """
+
+    psi = MPS(N)
+
+    for layer in circuit
+        for idx in 1:N÷2
+            gate = layer[idx]
+            site = 2*idx-1
+            apply_2site!(psi, gate, site)
+        end
+
+        for idx in N÷2+1:N-1
+            gate = layer[idx]
+            site = 2*idx - N
+            apply_2site!(psi, gate, site)
+        end
+    end
+
+    return psi
+end
+
+function create_upper_state_mps(circuit, phi::MPS, N)::MPS
+    n_layer = length(circuit)
+    psi = conj_mps(phi)
+
+    for kk in n_layer:-1:1
+        layer = circuit[kk]
+
+        for idx in N-1:-1:N÷2+1
+            gate = permutedims(layer[idx], (2,1))
+            site = 2*idx - N
+            apply_2site!(psi, gate, site)
+        end
+
+        for idx in N÷2:-1:1
+            gate = permutedims(layer[idx], (2,1))
+            site = 2*idx-1
+            apply_2site!(psi, gate, site)
+        end
+    end
+
+    return psi
+end
+
+@inline function polar_optimise_iteration_mps(gate, upper_state, lower_state, site, N)
+
+    apply_2site!(upper_state, conj(gate), site)
+
+    left_env = reshape([1], (1,1))
+    right_env = reshape([1], (1,1))
+
+    upper_tensors = upper_state.tensors
+    lower_tensors = lower_state.tensors
+
+    for ii in 1:site-1
+        @tensor left_env[vru,vrl] := left_env[vlu, vll] * upper_tensors[ii][vlu, p, vru] * lower_tensors[ii][vll, p, vrl]
+    end
+
+    for ii in N:-1:site+2
+        @tensor right_env[vlu,vll] := upper_tensors[ii][vlu, p, vru] * lower_tensors[ii][vll, p, vrl] * right_env[vru, vrl]
+    end
+
+    @tensor theta_upper[vl, p1, p2, vr] := upper_tensors[site][vl, p1, vc] * upper_tensors[site+1][vc, p2, vr]
+    @tensor theta_lower[vl, p1, p2, vr] := lower_tensors[site][vl, p1, vc] * lower_tensors[site+1][vc, p2, vr]
+
+    @tensor E[p2u, p1u, p2l, p1l] := left_env[vlu, vll] * theta_upper[vlu, p1u, p2u, vru] * theta_lower[vll, p1l, p2l, vrl] * right_env[vru, vrl]
+
+    E = reshape(E, (4, 4))
+    E = svd(E)
+    U = conj(E.U * E.Vt)
+
+    apply_2site!(lower_state, U, site)
+
+    return U, upper_state, lower_state
+    
+end
+
+export polar_optimise_mps
+function polar_optimise_mps(circuit, psi_GS::MPS, H_mpo::MPO, N; iterations=100)
+    n_layers = length(circuit)
+
+    overlaps = []
+    energies = []
+
+    iter = ProgressBar(1:iterations)
+    for iteration in iter
+
+        lower_state = MPS(N)
+        upper_state = create_upper_state_mps(circuit, psi_GS, N)
+
+        for kk in 1:n_layers
+            layer = circuit[kk]
+
+            for idx in 1:N÷2
+                gate = layer[idx]
+                site = 2*idx-1
+
+                circuit[kk][idx], upper_state, lower_state = polar_optimise_iteration_mps(gate, upper_state, lower_state, site, N)
+            end
+
+            for idx in N÷2+1:N-1
+                gate = layer[idx]
+                site = 2*idx - N
+
+                circuit[kk][idx], upper_state, lower_state = polar_optimise_iteration_mps(gate, upper_state, lower_state, site, N)
+            end
+        end
+
+        append!(overlaps, overlap(psi_GS, lower_state))
+        append!(energies, real(expectation(lower_state, H_mpo)))
+
+        set_multiline_postfix(iter, "energy: $(energies[end])")
+
+    end
+
+    return circuit, overlaps, energies
 
 end
